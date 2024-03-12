ca2d740a4a233ad2d60ab75ccd3252cb36ba91f5
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-03-12 23:28:14 +0530

    JavaScript practice

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,16 +1,17 @@
-function getMilk(money)
+function getMilk(money, milkPrice) 
 
 {
     console.log("move right");
     console.log("Move left");
-    var milkPrice = 66;
+    
     if (money >= milkPrice) {
       var milk =  money/milkPrice;}
       else {console.log ("You don't have enough money.")}
      milk =  Math.floor(milk);
+
      return ("You got " + milk + " bottle of milk.");
 
 
 }
-getMilk(500);
+getMilk(500, 66);
 
