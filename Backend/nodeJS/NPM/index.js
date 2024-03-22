15aa7a3d584b15cd39d65cc0c61dcdcd9f8be9aa
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-03-22 15:14:24 +0530

    node and npm

---
+++ Backend/nodeJS/NPM/index.js
@@ -0,0 +1,15 @@
+//Random silly name
+// npm sillyname
+//npm superheroes
+
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log('My name is ' + sillyName);
+
+// Superhero name
+import superheroes from "superheroes";
+var superheroName = superheroes.random();
+
+console.log("My superhero name is " +  superheroName); 
