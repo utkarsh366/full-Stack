b93cee116eceb865e63bc115a177659864e84218
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-03-23 22:38:44 +0530

    Node and express

--- Backend/http-request/index.js
+++ Backend/http-request/index.js
@@ -2,6 +2,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+app.get("/", (req,res) => {
+res.send("<h1>  hello world</h1>")});
+
+app.get("/about", (req, res) => {
+    res.send("<h1> hello </h1>")
+});
+app.get("/contact", (req, res) => {
+    res.send("Hola!")
+});
+
 app.listen(port, ()=>{
     console.log("server is started " + port)
 })
