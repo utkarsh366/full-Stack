dbdcc2b451f093af08aebd53bd59e04068633533
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-03-27 23:35:36 +0530

    Node and express

--- Backend/nodeJS/Middleware/index3.js
+++ Backend/nodeJS/Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("req method:", req.method);
+  console.log("req url", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
@@ -12,3 +18,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
