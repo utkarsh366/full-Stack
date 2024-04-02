9f3d5d362540ad01529fb65bd8dfa2a2830d3f38
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-04-02 23:11:11 +0530

    removing node module

---
+++ Backend/nodeJS/EJS/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = new Date();
+const currentWeekday = weekdays[today.getDay()];
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send(`<h1> Today is ${currentWeekday}.</h1>`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/ and listening for requests`);
+});
