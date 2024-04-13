d3f15ea64719d7dfbadc640c156c576d871822f2
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-04-13 19:18:25 +0530

    APIs-json

---
+++ Backend/APIs/API-Authentication/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/";
+
+// Fill in your values for the 3 types of auth.
+const yourUsername = "your_username";
+const yourPassword = "your_password";
+const yourAPIKey = "your_api_key";
+const yourBearerToken = "your_bearer_token";
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index", { content: JSON.stringify(result.data) });
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index", { content: JSON.stringify(result.data) });
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
