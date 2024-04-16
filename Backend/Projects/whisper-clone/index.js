011932b67e40b367666a3ce9558ae92a582bb283
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-04-16 17:00:06 +0530

    Whisper-clone

--- Backend/Projects/whisper-clone/index.js
+++ Backend/Projects/whisper-clone/index.js
@@ -1,13 +1,28 @@
-// HINTS:
-// 1. Import express and axios
 
-// 2. Create an express app and set the port number.
+import express from "express";
+import axios from "axios";
 
-// 3. Use the public folder for static files.
+const app = express();
+const port = 3000;
 
-// 4. When the user goes to the home page it should render the index.ejs file.
+app.use(express.static("public"));
 
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
 
-// 6. Listen on your predefined port and start the server.
+app.get("/", async (req,res) => {
+    try{
+
+    
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+        secret : result.data.secret,
+        user : result.data.username,
+    });
+}
+    catch(error){
+        console.log(Error.response.data);
+        res.status(500);
+    {}
+
+});
+
+app.listen(port, ()=>console.log(`Listening on port ${port}`));
