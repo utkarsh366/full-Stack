6c2da254d2f389c67d05d6915549c9135877b56c
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-04-28 12:57:56 +0530

    SQL

--- Database/SQL.sql
+++ Database/SQL.sql
@@ -23,4 +23,52 @@ SELECT *  FROM products;
 SELECT price, id FROM products;
 
                       --Reading with WHERE clause
-SELECT * FROM products WHERE price > 1.
+SELECT * FROM products WHERE id  = 1;
+
+--Updating
+UPDATE products SET price=5.99 WHERE id=1;
+
+--Deleting
+DELETE FROM products WHERE id=3;
+
+--Adding new column in existing table 
+ALTER TABLE products 
+ADD stock int;
+--updating the new column
+UPDATE products
+SET stocks = 12
+WHERE  id = 1;
+
+        --DELETE
+DELETE FROM products
+WHERE id = 2
+
+/*
+In CURD Operations:
+-C stands for Create which is used to add new records or data into the database.
+-U stands for Update which is used to modify existing records or data in the database.
+-R stands for Read which is used to retrieve information from the database. This operation does not change any data but displays it on screen.
+-R stands for Read which is used to fetch the record or data from the database.
+-D stands for Delete which is used to remove records or data from the database.
+*/
+
+
+
+-- join 
+ --creating a customer table
+ 
+
+
+
+
+
+
+
+
+
+
+
+--inner join
+SELECT order.order_no, cusomer.first_name, customers.last_name, customers.address
+from 'order'
+INNER join customer or order.customer_id = customer_id
