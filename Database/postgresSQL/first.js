5aa0d7959b76e871631545427018dc65344c608b
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-05-05 12:09:58 +0530

    quiz-postgres-database-project

--- Database/postgresSQL/first.js
+++ Database/postgresSQL/first.js
@@ -1,1 +1,20 @@
-import 
+import pg from "pg";
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "0987654321",
+    port: "5432",
+});
+
+db.connect();
+
+db.query("SELECT * FROM capitals", (err,res)=> {
+    if(err){
+        console.error("Error executing query", err.stack);
+    } else {
+        quiz = res.rows;
+    }
+    db.end();
+});
