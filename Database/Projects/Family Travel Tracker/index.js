61ec221087e583bd8d5437257354b9befb0d723d
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-05-16 12:17:37 +0530

    Project-family-travel-tracking-completed

--- Database/Projects/Family Travel Tracker/index.js
+++ Database/Projects/Family Travel Tracker/index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "0987654321",
+  password: "*****",//enter passsword
   port: 5432,
 });
 db.connect();
