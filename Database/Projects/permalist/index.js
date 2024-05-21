8cc6b41e0d7412464ee7122cd7ed3af8103c89c7
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-05-21 05:39:59 +0530

    Project-nodepad

---
+++ Database/Projects/permalist/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const Client = new Client({
+  user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: "0987654321",
+    port: 5432,
+});
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
