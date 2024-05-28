9cbc951e44077a253425186af935bc19c4902db6
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-05-28 08:42:52 +0530

    Project- authetication

--- Backend/Authentication/Authentication+Lv.1/index.js
+++ Backend/Authentication/Authentication+Lv.1/index.js
@@ -73,6 +73,25 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req,res)=>{
    const email = req.body.username;
    const password = req.body.password;
+
+   try{
+     const result = await db.query("SELECT * FROM users WHERE email = $1", [email,]);
+     if(result.rows.length > 0){
+      const user = result.rows[0];
+      const storedPassword = user.password;
+ 
+      if(password === storedPassword){
+        res.render("secrets.ejs");
+      }
+     }
+     else {
+      res.send("User not found");
+     }
+     
+   }
+   catch(err){
+    console.log(err);
+      }
 });
 
 app.listen(port, () => {
