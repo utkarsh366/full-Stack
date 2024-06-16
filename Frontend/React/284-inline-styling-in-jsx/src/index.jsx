66a1e1c118f1ecc3e79cfc34ee60e83ed812cbf0
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-06-16 21:23:37 +0530

    inline css inreactt-

--- Frontend/React/284-inline-styling-in-jsx/src/index.jsx
+++ Frontend/React/284-inline-styling-in-jsx/src/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle ={
+    color: "red";
+    fontSize: "20px";
+    border: "1px solid black";
+    padding: "10px";
+};
+ReactDOM.render(<h1 style={customStyle}>Hello World!</h1>, document.getElementById("root"));
+
 
 // If you're running this locally in VS Code use the commands:
 // npm install
