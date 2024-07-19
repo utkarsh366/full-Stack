52b9a770904d7aa325619eb332699353c0385d23
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-07-19 10:33:44 +0530

    React from youtube practice for inteview

---
+++ Frontend/React/youtube/Youtube-practice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 p-4">
+      <div className="w-44 h-32 bg-red-600 p-3 rounded-xl">
+        <h1>hey
+        </h1>
+      </div>
+    </div>
+  );
+} 
+
+export default App
