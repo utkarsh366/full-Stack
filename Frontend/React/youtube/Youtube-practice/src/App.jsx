8ba409555df692b621545ccd5bb88a278334811f
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-07-20 09:04:25 +0530

    React from youtube practice for inteview

--- Frontend/React/youtube/Youtube-practice/src/App.jsx
+++ Frontend/React/youtube/Youtube-practice/src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+ var [a,b] = useState(23);
   return (
-    <div className="w-full h-screen bg-zinc-900 p-4">
-      <div className="w-44 h-32 bg-red-600 p-3 rounded-xl">
-        <h1>hey
-        </h1>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      <h1>{a+2}</h1>
+      <button className='px-3 py-1 bg-green-500 rounded-md text-xs'> Click </button>
       </div>
-    </div>
-  );
-} 
+    
+  )
+}
 
 export default App
---
+++ Frontend/React/youtube/Youtube-practice/src/products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className=''>
+      <div className='w-full h-[22.5rem] bg-zinc-800'></div>
+    </div>
+  )
+}
+
+export default Products
