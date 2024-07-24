4d69fb3e3c165844a7c66e8d58241ab63020eceb
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-07-24 18:46:15 +0530

    React learning

---
+++ Frontend/React/youtube-course/day-2-react/bootstrap/bootstrap-project-practice/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-primary">Left</button>
+    <button type="button" class="btn btn-primary">Middle</button>
+    <button type="button" class="btn btn-primary">Right</button>
+  </div>
+  )
+}
+
+export default App
