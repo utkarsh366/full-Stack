5652d689da2837cce144ffdfa7fe39888ae5f754
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-07-29 15:03:11 +0530

    react learning

--- Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
+++ Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
@@ -1,13 +1,16 @@
-
 function App() {
-  
-
-  return 
+  return;
   {
-    <center class="todo-customer ">
-    <h1>TODO App</h1> </center>
+    <center class="todo-container ">
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-8">col-8</div>
+          <div class="col-4">col-4</div>
+        </div>
+      </div>
+    </center>;
   }
-  ;
 }
 
-export default App
+export default App;
