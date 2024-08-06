faa1e9a3bfb223c7f1c3eec98101027eba0a6554
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-08-06 21:11:08 +0530

    react learning

--- Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
+++ Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
@@ -1,16 +1,36 @@
+import AppName from "./Components/appName";
+import AddToDo from "./Components/AddToDo";
 function App() {
-  return;
-  {
-    <center class="todo-container ">
-      <h1>TODO App</h1>
+  return (
+    <center class="todo-container">
+      <AppName />
+      <AddToDo />
+
       <div class="container text-center">
         <div class="row">
-          <div class="col-8">col-8</div>
-          <div class="col-4">col-4</div>
+          <div class="col-6">Buy Milk </div>
+          <div class="col-4">11/04/2024 </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </center>;
-  }
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">GoTo college </div>
+          <div class="col-4">11/04/2024 </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
---
+++ Frontend/React/projects/TO-Do/ToDo-app-version-one/src/Components/appName.jsx
@@ -0,0 +1,4 @@
+function AppName() {
+  return <h1>ToDo React App</h1>;
+}
+export default AppName;
