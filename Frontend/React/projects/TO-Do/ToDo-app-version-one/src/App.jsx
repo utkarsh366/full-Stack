2e5a616fb653d4ca7eb55f35f0bb4ddf92da4e30
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-08-12 21:39:08 +0530

    react learning

--- Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
+++ Frontend/React/projects/TO-Do/ToDo-app-version-one/src/App.jsx
@@ -1,10 +1,11 @@
 import AppName from "./Components/appName";
 import AddToDo from "./Components/AddToDo";
+import ToDoitem from "./Components/ToDoitem";
+
 function App() {
   return (
     <center class="todo-container">
       <AppName />
-      <AddToDo />
       <AddToDo />
 
       <div class="container text-center">
@@ -18,18 +19,7 @@ function App() {
           </div>
         </div>
       </div>
-
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">GoTo college </div>
-          <div class="col-4">11/04/2024 </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
+      <ToDoitem />
     </center>
   );
 }
