f2309dffb90971e6d6bb6a0b09bd806372e2a31c
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-08-17 08:38:25 +0530

    react learning

--- Frontend/React/Fragments/Learning fragments/src/App.jsx
+++ Frontend/React/Fragments/Learning fragments/src/App.jsx
@@ -1,7 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 
 function App() {
-  return <div>fragmetns</div>;
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+        <li class="list-group-item">Porta ac consectetur ac</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
