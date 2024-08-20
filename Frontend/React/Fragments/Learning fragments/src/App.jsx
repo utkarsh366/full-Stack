86c06272851ca4115215fff0f729d93cd991e1e1
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-08-20 21:10:38 +0530

    react learning

--- Frontend/React/Fragments/Learning fragments/src/App.jsx
+++ Frontend/React/Fragments/Learning fragments/src/App.jsx
@@ -3,15 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  let foodItems = ["Dal", "Green Vegetables", "Roti"];
   return (
     <>
       <h1>Healthy Food</h1>
       <ul class="list-group">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
+        {foodItems.map((items) => (
+          <li class="list-group-item">{items}</li>
+        ))}
       </ul>
     </>
   );
