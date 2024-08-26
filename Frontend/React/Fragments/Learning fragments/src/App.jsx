2766fb37d59f34d8d1c9c6f631fc9c25527f4484
Utkarsh Sharma <utkarshsharma366@gmail.com>
2024-08-26 21:36:35 +0530

    learning conditional formatting in react

--- Frontend/React/Fragments/Learning fragments/src/App.jsx
+++ Frontend/React/Fragments/Learning fragments/src/App.jsx
@@ -3,13 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  let foodItems = ["Dal", "Green Vegetables", "Roti"];
+  // let foodItems = ["Dal", "Green Vegetables", "Roti"];
+
+  let foodItems = [];
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 ? <h3> I'm still hingry</h3> : null}
       <ul class="list-group">
         {foodItems.map((items) => (
-          <li clasName="list-group-item">{items}</li>
+          <li key={items} clasName="list-group-item">
+            {items}
+          </li>
         ))}
       </ul>
     </>
